b2ff21d64ea50af1b1662b2105c93641fa74e24a
Leo <codeunshackled@gmail.com>
2017-09-15 15:25:11 -0300

    Fixed host in environment production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "sample_app_#{Rails.env}"
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'mainherokuapp.herokuapp.com'
+  host = 'maintestapp.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
