b28b449ff336524a1a0a631f31fa87396254eced
Leo <codeunshackled@gmail.com>
2017-09-15 15:32:58 -0300

    Fixed again seeds config

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
+             email: "codeunshackled@gmail.com",
              password:              "foobar",
              password_confirmation: "foobar",
              admin: true,
@@ -8,7 +8,7 @@ User.create!(name:  "Example User",
 
 99.times do |n|
   name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
+  email = "example-#{n+1}@rortutorial.org"
   password = "password"
   User.create!(name:  name,
                email: email,
